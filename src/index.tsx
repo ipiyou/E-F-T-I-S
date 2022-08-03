992fc95030e76ce2988e62b946ac4080b4ae9559
ipiyou <tao710803@gmail.com>
2022-08-03 23:15:28 +0900

    global style 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'
+import GlobalStyle from './styled/global';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
 
