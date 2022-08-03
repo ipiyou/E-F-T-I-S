2b2e944f41ceed28117a1b862151596f56a57f6a
ipiyou <tao710803@gmail.com>
2022-08-03 23:11:42 +0900

    change apis

---
+++ src/apis/Get_Item_All.ts
@@ -0,0 +1,25 @@
+import { request, gql } from "graphql-request";
+
+export const GetItemAll = () => {
+  const query = gql`
+    {
+      items(name: "paca") {
+        name
+        types
+        basePrice
+        width
+        height
+        changeLast48hPercent
+        iconLink
+        sellFor {
+          price
+          source
+        }
+      }
+    }
+  `;
+
+  return request("https://api.tarkov.dev/graphql", query);
+};
+
+GetItemAll().then(data => console.log(data.items[0]))
