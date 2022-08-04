0ab8db31723e072ec0e2cadfe131711c4dccea22
ipiyou <tao710803@gmail.com>
2022-08-04 23:18:45 +0900

    style,hook 적용

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,45 +1,65 @@
 import styled from "@emotion/styled";
-import useSearcher from "../hooks/useSearcher";
+import useRecent from "../hooks/useRecent";
+import useForm from "../hooks/useForm";
+import SearchIcon from "../assets/Search";
+import { FormEvent } from "react";
+import ReMoveIcon from '../assets/Back'
+
+interface useFormType {
+  searchValue: string;
+}
 
 function SearchBox() {
-    const {Text,handleChange} = useSearcher()
+  const { Text, handleChange } = useForm<useFormType>({ searchValue: "" });
+  const { recent, AddRecent, DelRecent } = useRecent();
+
+  const ClickIcon = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    AddRecent(Text.searchValue);
+  };
   return (
-    <SearchContainer>
-      <InputContainer>
-        <Input value={Text} onChange={handleChange}/>
-        <SearchImg src="Search.svg" />
-      </InputContainer>
-      <RecentContainer>
-        
-      </RecentContainer>
-    </SearchContainer>
+    <_Wrapper>
+      <_InputWrapper onSubmit={ClickIcon}>
+        <_SearchInput
+          name="searchValue"
+          onChange={handleChange}
+        />
+        <button style={{all: 'unset'}}>
+          <SearchIcon />
+        </button>
+      </_InputWrapper>
+      <_RecentWrapper>
+        {recent.map((data) => (
+          <div>{data}<ReMoveIcon /></div>
+        ))}
+      </_RecentWrapper>
+    </_Wrapper>
   );
 }
 
-const SearchContainer = styled.div`
+const _Wrapper = styled.div`
   width: 560px;
   min-height: 56px;
   background-color: white;
   border-radius: 8px;
-  
 `;
 
-const RecentContainer = styled.div`
-    padding: 0 30px 0 30px;
-`
+const _RecentWrapper = styled.div`
+  padding: 0 30px;
+`;
 
-const InputContainer = styled.div`
-  padding: 0 30px 0 30px;
+const _InputWrapper = styled.form`
+  padding: 0 30px;
   display: flex;
   align-items: center;
 `;
 
-const Input = styled.input`
+const _SearchInput = styled.input`
   width: 100%;
   height: 50px;
 `;
 
-const SearchImg = styled.img`
+const _SearchImg = styled.img`
   width: 25px;
   height: 25px;
 `;
