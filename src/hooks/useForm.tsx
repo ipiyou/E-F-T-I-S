7697f58697363f2fb03db29e5cedeffe87dbbce7
ipiyou <tao710803@gmail.com>
2022-08-04 23:19:01 +0900

    hook 재사용

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent, useState } from "react";
+
+function useForm<T>(value: T) {
+  const [Text, setText] = useState<T>(value);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setText({ ...Text, [name]: value });
+  };
+  return {
+    Text,
+    handleChange,
+  };
+}
+
+export default useForm;
