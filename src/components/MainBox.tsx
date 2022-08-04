11369b4034557310aeee05e41bc22b12c3870b9d
ipiyou <tao710803@gmail.com>
2022-08-04 23:18:11 +0900

    type 변경

--- src/components/MainBox.tsx
+++ src/components/MainBox.tsx
@@ -1,25 +1,26 @@
 import styled from "@emotion/styled";
-import { FunctionComponent } from "react";
 
 interface PropType {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const MainBox: FunctionComponent<PropType> = ({children}) => {
-    return(
-        <MainContainer>
-            <Title>E  F  T  I  S</Title>
-            {children}
-        </MainContainer>
-    );
-}
+const MainBox = ({ children }: PropType) => {
+  return (
+    <MainContainer>
+      <Title>E F T I S</Title>
+      {children}
+    </MainContainer>
+  );
+};
 
 const MainContainer = styled.div`
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Title = styled.span`
-    font-size: 60px
-`
+  font-size: 60px;
+`;
+
+export default MainBox;
