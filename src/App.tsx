47a71040bbb973ae883a691f82f29840ff30df47
ipiyou <tao710803@gmail.com>
2022-08-06 02:20:53 +0900

    Search input hook와 검색중 상태 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import MainBox from "./components/MainBox";
-import ItemBox from "./components/ItemBox";
+import ItemBox from "./components/ItemList/ItemBox";
 import SearchBox from "./components/SearchBox";
+import useForm from "./hooks/useForm";
+import { useState } from "react";
+
+interface useFormType {
+  searchValue: string;
+}
 
 function App() {
+  const [Search,setSearch] = useState<boolean>(true);
+  const { Text, setText, handleChange } = useForm<useFormType>({
+    searchValue: "",
+  });
   return (
     <MainBox>
-      <SearchBox />
-      <ItemBox />
+      <SearchBox setSearch={setSearch} Text={Text} setText={setText} handleChange={handleChange}/>
+      <ItemBox Search={Search} setSearch={setSearch} Text={Text}/>
     </MainBox>
   );
 }
