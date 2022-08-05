69bc10212b22bff68b7c9aa26a94a0072961318d
ipiyou <tao710803@gmail.com>
2022-08-05 23:55:22 +0900

    불필요한 변수 삭제

--- src/hooks/useRecent.tsx
+++ src/hooks/useRecent.tsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
 
-interface useFormType {
-  searchValue: string;
+interface PropType {
+  setSearch: (Search: boolean) => void;
 }
 
-function useRecent() {
+function useRecent({ setSearch }: PropType) {
   const local = localStorage.getItem("recent");
-
   const [recent, setRecent] = useState<string[]>(
     local ? JSON.parse(local) : []
   );
   const AddRecent = (str: string) => {
-    let RECENTIN = [...recent];
-    if (RECENTIN.includes(str)) {
+    if (recent.includes(str)) {
       let arr = recent.filter((e) => e !== str);
       arr.unshift(str);
       setRecent(arr);
       localStorage.setItem("recent", JSON.stringify(arr));
+      setSearch(true);
     } else if (str !== "") {
-      RECENTIN.push(str);
-      setRecent(RECENTIN);
-      localStorage.setItem("recent", JSON.stringify(RECENTIN));
+      recent.push(str);
+      setRecent(recent);
+      localStorage.setItem("recent", JSON.stringify(recent));
+      setSearch(true);
     }
   };
   const DelRecent = (str: string) => {
-    let RECENT = [...recent];
-    let arr = RECENT.filter(e => e !== str)
+    let arr = recent.filter((e) => e !== str);
     setRecent(arr);
     localStorage.setItem("recent", JSON.stringify(arr));
   };
+
   return {
     recent,
     AddRecent,
